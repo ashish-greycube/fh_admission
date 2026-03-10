0a5512b1849c8c4169fc27835ac2d1cf9ce6e4e5
jigar-greycube <jigar@greycube.in>
2026-03-10 09:54:50 +0530

    feat:added fh_admission role in all doctypes

--- fh_admission/fh_admission/doctype/inquiry_form_fh/inquiry_form_fh.py
+++ fh_admission/fh_admission/doctype/inquiry_form_fh/inquiry_form_fh.py
@@ -3,26 +3,5 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.website.website_generator import WebsiteGenerator
-from frappe import _
-
-
 class InquiryFormFH(Document):
 	pass
-
-
-# class InquiryFormFH(WebsiteGenerator):
-
-# 	website = frappe._dict(
-# 		template = "templates/generators/inquiry.html",
-# 		condition_field = "is_published_field",
-# 		page_title_field = "job_title",
-# 	)
-
-# 	def get_context(self, context):
-# 		# show breadcrumbs
-# 		context.parents = [{'name': 'inquiry_form_fh', 'title': _('All Jobs') }]
-			
-# 	def validate(self):
-# 		self.route='inquiry/'+self.name
-# 		# reccomedation_calculator(self)
